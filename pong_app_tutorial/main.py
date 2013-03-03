598339d2d1d4353f0925c533a7ac8a9187de29c2
Thomas Kierzkowski <tjkierzkowski@gmail.com>
2013-03-03 11:57:11 -0500

    Added the ball animation
    See this tutorial page for details
    <http://kivy.org/docs/tutorials/pong.html#doc-adding-ball-animation>

--- pong_app_tutorial/main.py
+++ pong_app_tutorial/main.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 class PongBall(Widget):
-	"""Creates the ball used within the PongGame"""
+	"""Creates the ball properties used within the PongGame"""
 	#velocity of the ball in the x and y axis
-	velocity_x = NumericProperty(0)
+	velocity_x = NumericProperty(1)
 	velocity_y = NumericProperty(0)	
 	# referencelist property so we can use ball.velocity as
     # a shorthand..just like e.g. w.pos for w.x and w.y
@@ -18,12 +20,32 @@ class PongBall(Widget):
 		self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
-	pass
+	ball = ObjectProperty(None)
+
+	def serve_ball(self):
+		self.ball.center = self.center
+		self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+	
+	def update(self, dt):
+		#redraws the scene (i.e. calls ball.move and other objects animations)
+		self.ball.move()
+
+		#bounce off the top and bottom
+		if(self.ball.y < 0) or (self.ball.top > self.height):
+			self.ball.velocity_y *= -1
+
+		#bounce off left and right
+		if (self.ball.x < 0) or (self.ball.right > self.width):
+			self.ball.velocity_x *= -1
 
 
 class PongApp(App):
+
 	def build(self):
-		return PongGame()
+		game = PongGame()
+		game.serve_ball()
+		Clock.schedule_interval(game.update, 1.0/60.0)
+		return game
 
 
 if __name__ == '__main__':
